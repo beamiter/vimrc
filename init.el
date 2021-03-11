59c94c0b3107e6d3a8d13a0c9e877207611b1a1d
beamiter <beamiter@163.com>
2021-03-11 17:12:00 +0800

    update theme

--- init.el
+++ init.el
@@ -152,6 +152,9 @@
       map))
 (use-package winum)
 (winum-mode)
+(use-package evil-easymotion
+  :config
+  (evilem-default-keybindings "`"))
 
 ;; global-set-key
 (global-set-key (kbd "C-x C-b") 'ibuffer)
@@ -207,9 +210,9 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-vibrant t)
+  (load-theme 'doom-material t)
   ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
+  ;(doom-themes-visual-bell-config)
   (doom-themes-org-config))
 
 ;; -------- ui --------
