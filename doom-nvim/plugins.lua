3c795f556e0b54464c92cd9c6e432dd37e70bced
beamiter <beamiter@163.com>
2021-08-13 09:25:34 +0000

    fzf

--- doom-nvim/plugins.lua
+++ doom-nvim/plugins.lua
@@ -27,5 +27,11 @@ return {
       vim.g.EasyMotion_do_mapping = 0
       vim.g.EasyMotion_smartcase = 1
     end,
+  },
+  {
+    'junegunn/fzf', dir = '~/.fzf', run = './install --all'
+  },
+  {
+    'junegunn/fzf.vim'
   }
 }
