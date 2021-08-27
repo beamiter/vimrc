339aa12646766dff9bf5e99ef1b1b81a4e3bc9ab
yinj3 <yinj3@xiaopeng.com>
2021-08-27 18:30:13 +0800

    start to eat my own xmonad rice

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -1,14 +1,11 @@
 import XMonad
-
---main :: IO ()
---main = xmonad def
-
-import XMonad
 import XMonad.Hooks.DynamicLog (xmobar)
+import XMonad.Util.SpawnOnce
 
 myConfig = def
   { modMask     = mod1Mask -- set 'Mod' to windows key
   , terminal    = "gnome-terminal" -- for Mod + Shift + Enter
+  , startupHook = spawnOnce "bash ~/.fehbg &"
   }
 
 main = xmonad =<< xmobar myConfig
