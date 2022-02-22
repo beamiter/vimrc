a5e610509e4c301daaafdb06a38f2d75d1a4f2e6
yinj3 <yinj3@xiaopeng.com>
2022-02-22 07:16:41 +0000

    add mine config for nvchad

---
+++ nvchad/custom/chadrc.lua
@@ -0,0 +1,26 @@
+-- Just an example, supposed to be placed in /lua/custom/
+
+local userPlugins = require "custom.plugins"
+
+local M = {}
+
+-- make sure you maintain the structure of `core/default_config.lua` here,
+-- example of changing theme:
+
+M.ui = {
+   --theme = "gruvchad",
+}
+
+M.plugins = {
+    install = userPlugins,
+    options = {
+       lspconfig = {
+          setup_lspconf = "custom.plugins.lspconfig",
+       },
+    },
+    status = {
+       alpha = true,
+    },
+}
+
+return M
