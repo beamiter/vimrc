08639f10f8ff045a5ccbcc8886565834a39b2273
yinj3 <yinj3@xiaopeng.com>
2022-02-22 11:48:01 +0000

    more nvchad config

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -5,7 +5,21 @@
 -- MAPPINGS
 local map = require("core.utils").map
 
-map("n", "<leader>cc", ":Telescope <CR>")
+map("n", "<leader><leader>", ":Telescope <CR>")
 map("n", "<leader>q", ":q <CR>")
+map('n', 's', "<cmd>lua require'hop'.hint_char2()<CR>", {})
+map('n', 'S', "<cmd>lua require'hop'.hint_char1()<CR>", {})
+map('n', 'f', "<cmd>lua require'hop'.hint_char2({ current_line_only = true })<CR>", {})
+map('n', 'F', "<cmd>lua require'hop'.hint_char1({ current_line_only = true })<CR>", {})
+map("n", "<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>")
+map("n", "<leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>")
+map("n", "<leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>")
+map("n", "<leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>")
+map("n", "<leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>")
+map("n", "<leader>6", "<Cmd>BufferLineGoToBuffer 6<CR>")
+map("n", "<leader>7", "<Cmd>BufferLineGoToBuffer 7<CR>")
+map("n", "<leader>8", "<Cmd>BufferLineGoToBuffer 8<CR>")
+map("n", "<leader>9", "<Cmd>BufferLineGoToBuffer 9<CR>")
+
 
 -- NOTE: the 4th argument in the map function is be a table i.e options but its most likely un-needed so dont worry about it
--- nvchad/custom/plugins/init.lua
+++ nvchad/custom/plugins/init.lua
@@ -10,6 +10,14 @@ return {
           -- refer to the configuration section below
         }
       end
+   },
+   {
+      'phaazon/hop.nvim',
+      branch = 'v1', -- optional but strongly recommended
+      config = function()
+        -- you can configure Hop the way you like here; see :h hop-config
+        require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+      end
    }
 
 }
