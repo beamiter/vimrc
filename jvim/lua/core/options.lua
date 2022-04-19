4c87bc6bd63d3dc6d4f99d6589623b8b01fcbb96
yinj3 <yinj3@xiaopeng.com>
2022-04-19 06:33:28 +0000

    stole from others

--- jvim/lua/core/options.lua
+++ jvim/lua/core/options.lua
@@ -1,33 +1,49 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ','
+local opt = vim.opt
+local g = vim.g
 
-vim.opt.backspace={'indent','eol','start'}
-vim.opt.completeopt={'menu','menuone','noselect'}
+g.mapleader = ' '
+g.maplocalleader = ','
+g.noautochdir = true
+g.nobackup = true
+g.noswapfile = true
+g.nowritebackup = true
+g.vscode_style = "dark"
 
-vim.opt.clipboard:append 'unnamedplus'
--- vim.opt.shortmess:append 'csI'
-
-vim.opt.autoindent = true
-vim.opt.colorcolumn = '120'
-vim.opt.encoding= 'utf-8'
-vim.opt.expandtab = true
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.shiftwidth=4
-vim.opt.showtabline=2
-vim.opt.laststatus=2
-vim.opt.tabstop=4
-vim.opt.pumheight=10
-vim.opt.termguicolors = true
-vim.opt.signcolumn = 'yes'
-vim.opt.list = true
-vim.opt.scrolloff = 18
-vim.opt.sidescrolloff = 3
---vim.opt.listchars:append("eol:↴")
-
-vim.g.noautochdir = true
-vim.g.nobackup = true
-vim.g.noswapfile = true
-vim.g.nowritebackup = true
-vim.g.vscode_style = "dark"
-
+opt.autoindent = true
+opt.backspace={'indent','eol','start'}
+opt.clipboard:append 'unnamedplus'
+opt.cmdheight = 1
+opt.colorcolumn = '120'
+opt.completeopt={'menu','menuone','noselect'}
+opt.cul = true
+opt.encoding= 'utf-8'
+opt.expandtab = true
+opt.fillchars = { eob = " " }
+opt.hidden = true
+opt.ignorecase = true
+opt.laststatus=2
+opt.list = true
+opt.mouse = "a"
+opt.number = true
+opt.numberwidth = 2
+opt.pumheight=10
+opt.relativenumber = true
+opt.ruler = false
+opt.scrolloff = 18
+opt.shadafile = "NONE"
+opt.shiftwidth=2
+opt.showtabline=2
+opt.shortmess:append "sI"
+opt.signcolumn = 'yes'
+opt.sidescrolloff = 3
+opt.smartcase = true
+opt.smartindent = true
+opt.splitbelow = true
+opt.splitright = true
+opt.tabstop=4
+opt.termguicolors = true
+opt.title = true
+opt.timeoutlen = 800
+opt.updatetime = 250
+opt.undofile = true
+opt.whichwrap:append "<>[]hl"
--- jvim/lua/plugins/configs/nvim-lspconfig.lua
+++ jvim/lua/plugins/configs/nvim-lspconfig.lua
@@ -1,65 +1,92 @@
 local present, _ = pcall(require, "lspconfig")
 
 if not present then
-    return
+  return
 end
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[e', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']e', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(_, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>fm', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>fm', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.documentationFormat = { "markdown", "plaintext" }
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    "documentation",
+    "detail",
+    "additionalTextEdits",
+  },
+}
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
 local lsp_installer = require("nvim-lsp-installer")
--- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
--- or if the server is already installed).
+lsp_installer.settings {
+  ui = {
+    icons = {
+      server_installed = "﫟",
+      server_pending = "",
+      server_uninstalled = "✗",
+    },
+  },
+}
+
 lsp_installer.on_server_ready(function(server)
-    local server_opts = {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        flags = {
-            -- This will be the default in neovim 0.7+
-            debounce_text_changes = 150,
-        }
-    }
+  local server_opts = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+      debounce_text_changes = 150,
+    },
+    settings = {},
+  }
 
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
+  -- basic example to edit lsp server's options, disabling tsserver's inbuilt formatter
+  if server.name == 'tsserver' then
+    opts.on_attach = function(client, bufnr)
+      client.resolved_capabilities.document_formatting = false
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
+    end
+  end
 
-    -- This setup() function will take the provided server configuration and decorate it with the necessary properties
-    -- before passing it onwards to lspconfig.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    server:setup(server_opts)
+  server:setup(server_opts)
+
+  vim.cmd [[ do User LspAttachBuffers ]]
 end)
 
 -- diagnostic config
 local config = {
-    virtual_text = false,
+  virtual_text = false,
 }
 vim.diagnostic.config(config)
