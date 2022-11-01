36193df8550dfa64c27bcace3093e557e6e4da9a
yinj3 <yinj3@xiaopeng.com>
2022-11-01 17:40:29 +0800

    nvimtree

--- lvim/config.lua
+++ lvim/config.lua
@@ -21,6 +21,7 @@ vim.opt.relativenumber = true
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<F3>"] = ":NvimTreeToggle<cr>"
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
