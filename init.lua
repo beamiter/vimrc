9b1376e98e21f6af4713980374008ed0e5869cec
yinj3 <yinj3@xiaopeng.com>
2022-11-09 13:45:07 +0800

    nvim format

--- init.lua
+++ init.lua
@@ -739,8 +739,8 @@ require('packer').startup(function()
       buf_map(bufnr, 'n', '<space>ac', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
       buf_map(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
       buf_map(bufnr, 'n', '<space>fm', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
-      -- buf_map(bufnr, 'v', '<space>f', '<cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
-      -- buf_map(bufnr, 'v', '<space>bf', '<cmd>lua vim.lsp.buf.range_formatting()<CR>', opts)
+      buf_map(bufnr, 'x', '<space>fm', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
+      buf_map(bufnr, 'x', '<space>bf', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
 
       require 'illuminate'.on_attach(client)
     end
