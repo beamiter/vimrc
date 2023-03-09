2e04569cd6edc6e25fd821693d3c8ca4fd7fbf65
yinj3 <yinj3@xiaopeng.com>
2023-03-09 07:42:47 +0000

    onedarker

--- lvim/config.lua
+++ lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = false
-lvim.colorscheme = "dracula"
+lvim.colorscheme = "onedarker"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
