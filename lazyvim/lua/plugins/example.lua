7884127fb5812a7f2a5feadaf3e3e331df11e7ee
yinj3 <yinj3@xiaopeng.com>
2023-03-16 06:53:41 +0000

    eat lz dog rice

--- lazyvim/lua/plugins/example.lua
+++ lazyvim/lua/plugins/example.lua
@@ -28,7 +28,15 @@ return {
   },
 
   -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
+  { "folke/trouble.nvim",                          enabled = false },
+  { "JoosepAlviste/nvim-ts-context-commentstring", enabled = false },
+  { "echasnovski/mini.comment", enabled = false },
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  },
 
   -- add symbols-outline
   {
@@ -105,7 +113,7 @@ return {
       init = function()
         require("lazyvim.util").on_attach(function(_, buffer)
           -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
         end)
       end,
@@ -261,7 +269,7 @@ return {
   {
     's1n7ax/nvim-window-picker',
     config = function()
-        require'window-picker'.setup()
+      require 'window-picker'.setup()
     end,
   },
 
