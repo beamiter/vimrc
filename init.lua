b384e05a96860dd66bbd46d94f8e095dcff4446c
beamiter <beamiter@163.com>
2024-04-10 17:59:28 +0800

    rm

--- init.lua
+++ init.lua
@@ -602,20 +602,6 @@ require("lazy").setup({
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
     end,
-    keys = {
-      {
-        "<tab>",
-        function()
-          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = "i",
-      },
-      { "<tab>",   function() require("luasnip").jump(1) end,  mode = "s" },
-      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
-    },
-
   },
 
   {
