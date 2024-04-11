7c47f32ee3242455284488a4543b692d72168022
beamiter <beamiter@163.com>
2024-04-11 11:40:26 +0800

    more config

--- nvchad/starter/lua/plugins/init.lua
+++ nvchad/starter/lua/plugins/init.lua
@@ -9,11 +9,12 @@ return {
 
   {
     "junegunn/fzf",
-    build = "./install --bin"
+    build = "./install --bin",
   },
 
   {
-    "junegunn/fzf.vim", enabled = false
+    "junegunn/fzf.vim",
+    enabled = false,
   },
 
   {
@@ -23,29 +24,64 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       -- calling `setup` is optional for customization
-      require("fzf-lua").setup({})
-    end
+      require("fzf-lua").setup {}
+    end,
   },
 
   {
-    'phaazon/hop.nvim',
-    branch = 'v2', -- optional but strongly recommended
+    "phaazon/hop.nvim",
+    branch = "v2", -- optional but strongly recommended
     config = function()
       -- you can configure Hop the way you like here; see :h hop-config
-      require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-    end
+      require("hop").setup { keys = "etovxqpdygfblzhckisuran" }
+    end,
   },
 
   {
-    'akinsho/bufferline.nvim',
+    "akinsho/bufferline.nvim",
     version = "*",
-    dependencies = 'nvim-tree/nvim-web-devicons',
+    dependencies = "nvim-tree/nvim-web-devicons",
     lazy = false,
     config = function()
       require("bufferline").setup {}
-    end
-  }
+    end,
+  },
 
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      local function my_on_attach(bufnr)
+        local api = require "nvim-tree.api"
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+        -- default mappings
+        api.config.mappings.default_on_attach(bufnr)
+        -- custom mappings
+        vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts "Up")
+        vim.keymap.set("n", "?", api.tree.toggle_help, opts "Help")
+        -- global
+        -- vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", { silent = true, noremap = true })
+        -- on_attach
+        vim.keymap.set("n", "l", api.node.open.edit, opts "Edit Or Open")
+        vim.keymap.set("n", "h", api.node.navigate.parent_close, opts "Close Directory")
+        vim.keymap.set("n", "o", api.node.open.edit, opts "Open")
+        vim.keymap.set("n", "w", api.node.open.edit, opts "Open")
+        vim.keymap.set("n", "H", api.tree.collapse_all, opts "Collapse All")
+      end
+      require("nvim-tree").setup {
+        view = {
+          width = 60,
+        },
+        on_attach = my_on_attach,
+      }
+    end,
+  },
 
   -- These are some examples, uncomment them if you want to see them work!
   -- {
