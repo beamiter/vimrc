af55dbab26a4ee957c27763bb825fc4466d4fe72
beamiter <beamiter@163.com>
2024-04-15 23:01:32 +0800

    fuck

--- init.lua
+++ init.lua
@@ -151,10 +151,14 @@ require("lazy").setup({
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    opts = {},
-    config = function()
-      require("ibl").setup(
-      )
+    opts = {
+      indent = { char = "│" },
+      scope = { char = "│" },
+    },
+    config = function(_, opts)
+      local hooks = require "ibl.hooks"
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+      require("ibl").setup(opts)
     end,
   },
   { "nvim-neotest/nvim-nio" },
@@ -765,16 +769,6 @@ require("lazy").setup({
   "RRethy/vim-illuminate",
 
   {
-    "L3MON4D3/LuaSnip",
-    dependencies = { "rafamadriz/friendly-snippets" },
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-      require("luasnip.loaders.from_lua").lazy_load()
-      require("luasnip.loaders.from_snipmate").lazy_load()
-    end,
-  },
-
-  {
     "williamboman/mason.nvim",
     cmd = { "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
     config = function()
@@ -800,90 +794,180 @@ require("lazy").setup({
 
   {
     "hrsh7th/nvim-cmp",
-    dependencies = { 'saadparwaiz1/cmp_luasnip', 'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline' },
+    event = "InsertEnter",
+    dependencies = {
+      {
+        -- snippet plugin
+        "L3MON4D3/LuaSnip",
+        dependencies = "rafamadriz/friendly-snippets",
+        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+        config = function(_, opts)
+          require("luasnip").config.set_config(opts)
+
+          -- vscode format
+          -- require("luasnip.loaders.from_vscode").lazy_load { exclude = vim.g.vscode_snippets_exclude or {} }
+          -- require("luasnip.loaders.from_vscode").lazy_load { paths = vim.g.vscode_snippets_path or "" }
+
+          -- snipmate format
+          -- require("luasnip.loaders.from_snipmate").load()
+          require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snippets_path }
+
+          -- lua format
+          -- require("luasnip.loaders.from_lua").load()
+          -- require("luasnip.loaders.from_lua").lazy_load { paths = vim.g.lua_snippets_path or "" }
+
+          vim.api.nvim_create_autocmd("InsertLeave", {
+            callback = function()
+              if
+                  require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+                  and not require("luasnip").session.jump_active
+              then
+                require("luasnip").unlink_current()
+              end
+            end,
+          })
+        end,
+      },
+
+      -- autopairing of (){}[] etc
+      {
+        "windwp/nvim-autopairs",
+        opts = {
+          fast_wrap = {},
+          disable_filetype = { "TelescopePrompt", "vim" },
+        },
+        config = function(_, opts)
+          require("nvim-autopairs").setup(opts)
+
+          -- setup cmp for autopairs
+          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+      },
+
+      -- cmp sources plugins
+      {
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        'hrsh7th/cmp-cmdline',
+      },
+    },
     config = function()
-      -- Setup nvim-cmp.
-      local cmp = require("cmp")
-      local default = {
+      -- Set up nvim-cmp.
+      local cmp = require 'cmp'
+      local luasnip = require("luasnip")
+      cmp.setup({
         snippet = {
+          -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
           end,
         },
-        confirm_opts = {
-          select = true,
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
         },
-        completion = {
-          ---@usage The minimum length of a word to complete on.
-          keyword_length = 1,
-        },
-        formatting = {
-          format = function(entry, vim_item)
-            vim_item.menu = ({
-              nvim_lsp = "[LSP]",
-              nvim_lua = "[Lua]",
-              buffer = "[BUF]",
-            })[entry.source.name]
-            return vim_item
-          end,
-        },
-        mapping = {
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          -- ['<CR>'] = cmp.mapping(function(fallback)
+          --   if cmp.visible() then
+          --     if luasnip.expandable() then
+          --       luasnip.expand()
+          --     else
+          --       cmp.confirm({
+          --         select = true,
+          --       })
+          --     end
+          --   else
+          --     fallback()
+          --   end
+          -- end),
+          ["<CR>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+          },
           ["<C-p>"] = cmp.mapping.select_prev_item(),
           ["<C-n>"] = cmp.mapping.select_next_item(),
           ["<C-k>"] = cmp.mapping.select_prev_item(),
           ["<C-j>"] = cmp.mapping.select_next_item(),
           ["<UP>"] = cmp.mapping.select_prev_item(),
           ["<Down>"] = cmp.mapping.select_next_item(),
-          ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-          ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
           ["<C-y>"] = cmp.config.disable,
-          ["<C-e>"] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-          }),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
           ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            elseif jumpable(1) then
+            elseif luasnip.locally_jumpable(1) then
               luasnip.jump(1)
-            elseif has_words_before() then
-              -- cmp.complete()
-              fallback()
             else
               fallback()
             end
           end, { "i", "s" }),
+
           ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
+            elseif luasnip.locally_jumpable(-1) then
               luasnip.jump(-1)
             else
               fallback()
             end
           end, { "i", "s" }),
-        },
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "nvim_lua" },
-          { name = "path" },
-          { name = "nvim_lua" },
-          { name = "buffer" },
-          { name = "calc" },
-          { name = "emoji" },
-          { name = "treesitter" },
-          { name = "crates" },
-          { name = "tmux" },
-        },
-      }
+        }),
+        sources = cmp.config.sources({
+          { name = 'luasnip' }, -- For luasnip users.
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' },   -- For vsnip users.
+          -- { name = 'ultisnips' }, -- For ultisnips users.
+          -- { name = 'snippy' }, -- For snippy users.
+        }, {
+          { name = 'buffer' },
+        })
+      })
 
-      cmp.setup(default)
+      -- Set configuration for specific filetype.
+      cmp.setup.filetype('gitcommit', {
+        sources = cmp.config.sources({
+          { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+        }, {
+          { name = 'buffer' },
+        })
+      })
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false }
+      })
+
+      -- -- Set up lspconfig.
+      -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+      -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+      --   capabilities = capabilities
+      -- }
     end,
   },
 
@@ -895,7 +979,6 @@ require("lazy").setup({
       -- after the language server attaches to the current buffer
       local on_attach = function(client, bufnr)
         -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
         -- Mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
