a7c05be1717877f3d166f0032db2165d4cebb60d
beamiter <beamiter@163.com>
2024-05-10 16:01:36 +0800

    update astro
    Signed-off-by: beamiter <beamiter@163.com>

--- astronvim/template/lua/plugins/diy_plugins.lua
+++ astronvim/template/lua/plugins/diy_plugins.lua
@@ -10,11 +10,20 @@ return {
   },
   {
     "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
     opts = {
+      -- Configuration table of features provided by AstroLSP
       features = {
-        autoformat = false,
-      }
-    }
+        autoformat = false, -- enable or disable auto formatting on start
+      },
+      -- customize lsp formatting options
+      formatting = {
+        -- control auto formatting on save
+        format_on_save = {
+          enabled = false, -- enable or disable format on save globally
+        },
+      },
+    },
   },
   {
     "rebelot/heirline.nvim",
