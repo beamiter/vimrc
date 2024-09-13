e2df020876eff0354433a27ae36f9e48fc1d8fdf
beamiter <beamiter@163.com>
2024-09-13 11:02:13 +0800

    inlay hints

--- lazyvim/lua/plugins/example.lua
+++ lazyvim/lua/plugins/example.lua
@@ -107,6 +107,7 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
+      inlay_hints = { enabled = false },
       ---@type lspconfig.options
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
