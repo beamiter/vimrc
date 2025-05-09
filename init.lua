e7be4183d9a8c978d71ebdbd1cfc131cf4b75b2d
beamiter <beamiter@163.com>
2025-05-09 21:56:55 +0800

    config

--- init.lua
+++ init.lua
@@ -105,6 +105,7 @@ local opts = { noremap = true, silent = true }
 
 -- 文件浏览器
 map("n", "<F3>", ":NvimTreeFindFileToggle<CR>", opts)
+map("n", "<leader>e", ":NvimTreeFindFileToggle<CR>", opts)
 
 -- 窗口调整
 map("n", ")", ":vert res +5<CR>", opts)
@@ -415,10 +416,10 @@ require("lazy").setup({
         end
 
         -- 诊断相关
-        buf_map("n", "<space>e", vim.diagnostic.open_float, "diagnostic open")
+        buf_map("n", "<space>qo", vim.diagnostic.open_float, "diagnostic open")
         buf_map("n", "[d", vim.diagnostic.goto_prev, "prev diagnostic")
         buf_map("n", "]d", vim.diagnostic.goto_next, "next diagnostic")
-        buf_map("n", "<space>q", vim.diagnostic.setloclist, "diagnostic list")
+        buf_map("n", "<space>ql", vim.diagnostic.setloclist, "diagnostic list")
 
         -- LSP 导航
         buf_map("n", "gD", vim.lsp.buf.declaration, "declaration")
