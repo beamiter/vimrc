35cba5677533e48b9d64abf448ce666a9441ecd4
beamiter <beamiter@163.com>
2025-05-09 15:31:16 +0800

    use mason 1.0 currently

--- lazyvim/lua/plugins/example.lua
+++ lazyvim/lua/plugins/example.lua
@@ -1,6 +1,9 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
