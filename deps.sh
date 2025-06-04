3f59615cf82d5642c00aa0781a4e8e9a4daf86e1
beamiter <beamiter@163.com>
2025-06-04 22:20:09 +0800

    update deps

--- deps.sh
+++ deps.sh
@@ -1,1 +1,1 @@
-sudo apt-get install -y libfontconfig1-dev picom feh libx11-dev libxinerama-dev libxft-dev libxrender-dev npm libssl-dev dmenu flameshot
+sudo apt-get install -y libfontconfig1-dev picom feh libx11-dev libxinerama-dev libxft-dev libxrender-dev npm libssl-dev dmenu flameshot libasound2-dev xclip
